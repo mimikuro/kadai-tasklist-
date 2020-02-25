4f077ba6e99bd40e3e4b729b2e61fa4a59531777
takateru.kiguchi <takateru.kiguchi@gmail.com>
2020-02-25 14:45:35 +0900

    Add ShowServlet

--- src/models/Task.java
+++ src/models/Task.java
@@ -10,16 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 @Entity
 @NamedQueries({
     @NamedQuery(
-            name = "getAllMessages",
-            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            name = "getAllTasks",
+            query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
             )
 })
-@Table(name = "messages")
-public class Message {
+@Table(name = "tasks")
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
